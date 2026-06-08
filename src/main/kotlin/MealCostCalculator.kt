24613a8cc2070039aa088da0801a5b1bcbb46bbb
Daniil Dryzhov <danildryzov@gmail.com>
2026-06-08 14:10:21 +0200

    implement total meal cost func

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,1 +1,38 @@
-// create a function and implement it
+fun calculateTotalMealCost(
+    baseCost: Double,
+    taxRate: Double,
+    tipPercentage: Double,
+    discount: Double = 0.0
+): Double {
+    fun applyTax(cost: Double): Double {
+        return cost + (cost * taxRate)
+    }
+
+    fun applyTip(costWithTax: Double): Double {
+        return costWithTax + (costWithTax * tipPercentage)
+    }
+
+    fun applyDiscount(finalCost: Double): Double {
+        return if (discount > 0.0) {
+            finalCost - discount
+        } else {
+            finalCost
+        }
+    }
+
+    val costWithTax = applyTax(baseCost)
+    val costWithTip = applyTip(costWithTax)
+    val finalTotal = applyDiscount(costWithTip)
+
+    return finalTotal
+}
+
+fun main() {
+    val baseCost = 100.0
+    val taxRate = 0.1
+    val tipPercentage = 0.15
+    val discount = 10.0
+
+    val totalMealCost = calculateTotalMealCost(baseCost, taxRate, tipPercentage, discount)
+    println("Total meal cost: $$totalMealCost")
+}
